ac0d7d8c6e6b1cc2b454fba9881d564f622772b0
nicogreenarry <@1Y0#ZephFd0>
2020-05-19 08:34:28 -0700

    chore: Remove unused import variables

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
+import 'jquery';
+import 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from "../components/App";
 
