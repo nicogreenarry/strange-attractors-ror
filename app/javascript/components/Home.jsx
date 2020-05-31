1cf89206b829a3c5075d6a084391f02bebca8403
nicogreenarry <@1Y0#ZephFd0>
2020-05-30 17:33:27 -0700

    feat: Center text on homepage

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default () => (
   <div className="vw-100 vh-100 primary-color d-flex align-items-center justify-content-center">
     <div className="jumbotron jumbotron-fluid bg-transparent">
-      <div className="container secondary-color">
+      <div className="container secondary-color text-center">
         <h1 className="display-4">Strange Attractors</h1>
         <p className="lead">
           Math is beautiful!
