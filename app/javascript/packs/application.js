5ab06f302c373fc56cbb342be908272761f96a59
nicogreenarry <@1Y0#ZephFd0>
2020-05-30 19:12:19 -0700

    fix: Remove broken import

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,7 +4,6 @@
 // that code so it'll be compiled.
 
 import "bootstrap"
-import "../stylesheets/application"
 require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
