cd01ce73053edad202d5e0373a13a1eaa90777eb
nicogreenarry <@1Y0#ZephFd0>
2020-05-31 15:49:16 -0700

    refactor: Early return to reduce indentation

--- app/javascript/components/Attractor/index.jsx
+++ app/javascript/components/Attractor/index.jsx
@@ -48,31 +48,33 @@ const Attractor = (props) => {
   }, [props.coefficients]);
 
   useEffect(() => {
-    if (canvasEl.current) {
-      const ctx = canvasEl.current.getContext('2d');
-      ctx.fillStyle = 'green';
-
-      // TODO: Pull a lot of this logic out into services
-      const points = attractor.getPointsForRendering();
-      const {xMin, xMax, yMin, yMax} = points.reduce((bounds, [x, y]) => {
-        bounds.xMin = Math.min(bounds.xMin, x);
-        bounds.xMax = Math.max(bounds.xMax, x);
-        bounds.yMin = Math.min(bounds.yMin, y);
-        bounds.yMax = Math.max(bounds.yMax, y);
-        return bounds;
-      }, {xMin: Infinity, xMax: -Infinity, yMin: Infinity, yMax: -Infinity});
-      const attractorImageData = ctx.createImageData(WIDTH, HEIGHT);
-      points.forEach(([x, y]) => {
-        const [canvasX, canvasY] = getCanvasXYFromPoint({x, y, xMin, xMax, yMin, yMax});
-
-        const [redIdx, greenIdx, blueIdx, alphaIdx] = getColorIndicesForCoord(canvasX, canvasY, WIDTH);
-        attractorImageData.data[redIdx] = COLOR[0];
-        attractorImageData.data[greenIdx] = COLOR[1];
-        attractorImageData.data[blueIdx] = COLOR[2];
-        attractorImageData.data[alphaIdx] = COLOR[3];
-      });
-      ctx.putImageData(attractorImageData, 0, 0);
+    if (!canvasEl.current) {
+      return;
     }
+
+    const ctx = canvasEl.current.getContext('2d');
+    ctx.fillStyle = 'green';
+
+    // TODO: Pull a lot of this logic out into services
+    const points = attractor.getPointsForRendering();
+    const {xMin, xMax, yMin, yMax} = points.reduce((bounds, [x, y]) => {
+      bounds.xMin = Math.min(bounds.xMin, x);
+      bounds.xMax = Math.max(bounds.xMax, x);
+      bounds.yMin = Math.min(bounds.yMin, y);
+      bounds.yMax = Math.max(bounds.yMax, y);
+      return bounds;
+    }, {xMin: Infinity, xMax: -Infinity, yMin: Infinity, yMax: -Infinity});
+    const attractorImageData = ctx.createImageData(WIDTH, HEIGHT);
+    points.forEach(([x, y]) => {
+      const [canvasX, canvasY] = getCanvasXYFromPoint({x, y, xMin, xMax, yMin, yMax});
+
+      const [redIdx, greenIdx, blueIdx, alphaIdx] = getColorIndicesForCoord(canvasX, canvasY, WIDTH);
+      attractorImageData.data[redIdx] = COLOR[0];
+      attractorImageData.data[greenIdx] = COLOR[1];
+      attractorImageData.data[blueIdx] = COLOR[2];
+      attractorImageData.data[alphaIdx] = COLOR[3];
+    });
+    ctx.putImageData(attractorImageData, 0, 0);
   }, [canvasEl.current]);
 
   return <canvas ref={canvasEl} id="main-attractor" width={WIDTH} height={HEIGHT} />
