e937a6636419df8d376d805723192c3f75f92f8c
nicogreenarry <@1Y0#ZephFd0>
2020-06-01 09:15:48 -0700

    feat: Improved and refactored boringness check

--- app/javascript/services/attractor/find_interesting_coefficients.js
+++ app/javascript/services/attractor/find_interesting_coefficients.js
@@ -5,7 +5,12 @@ const COEFFICIENT_RANGE = 3;
 function createAttractorPoints() {
   const coefficients = Array(12).fill().map(() => Math.random() * COEFFICIENT_RANGE - COEFFICIENT_RANGE/2);
   const startingCoordinates = Array(2).fill().map(() => Math.random() * COEFFICIENT_RANGE - COEFFICIENT_RANGE/2);
-  return new AttractorPoints({coefficients, startingCoordinates, initialCount: 500});
+  return new AttractorPoints({
+    boringnessCheckFrequency: 10, // Randomly-generated sets are much more likely to get boring very fast
+    coefficients,
+    startingCoordinates,
+    initialCount: 500, // Should be plenty to discover whether the set is boring
+  });
 }
 
 export default function findInterestingCoefficients() {
