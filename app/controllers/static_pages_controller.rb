3de0023eb757c0f0ae2b7a2a6e827acf838dbfde
nicogreenarry <@1Y0#ZephFd0>
2020-06-06 16:05:15 -0700

    feat: Create What Are Strange Attractors static page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
   def about
   end
+
+  def what_are_strange_attractors
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'static_pages/about'
+  get 'static_pages/what_are_strange_attractors'
   root 'homepage#index'
 end
