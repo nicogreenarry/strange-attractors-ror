bba5512b22868255eab837ab884dd6f7ad9d3faf
nicogreenarry <@1Y0#ZephFd0>
2020-06-08 21:33:49 -0700

    refactor: Create full_title helper to simplify view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title = '')
+    base_title = "Strange Attractors"
+
+    # Use concatenation instead of interpolation because the call to `provide` converts the string into a SafeBuffer
+    # object instead of an ordinary string. Interpolating and inserting into a view template then over-escapes any
+    # inserted HTML, so a title such as “Help’s on the way” would be converted to “Help&amp;#39;s on the way”.
+    page_title.blank? ? base_title : page_title + " | " + base_title
+  end
 end
