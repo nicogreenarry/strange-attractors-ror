a0dd3ff67fd6783f68206ea57ac749c20946d861
nicogreenarry <@1Y0#ZephFd0>
2020-06-11 09:28:08 -0700

    feat: Generate Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'homepage#index'
+  get 'signup', to: 'users#new'
   get '/about', to: 'static_pages#about'
   get '/what-are-strange-attractors', to: 'static_pages#what_are_strange_attractors'
 end
