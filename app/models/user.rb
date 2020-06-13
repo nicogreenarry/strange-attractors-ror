6c1841c532af60bfa38c26f4dbbda3f312b1724d
nicogreenarry <@1Y0#ZephFd0>
2020-06-13 13:29:12 -0700

    feat: Add user password to model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,7 @@ class User < ApplicationRecord
             format: { with: /.+@.+/ },
             uniqueness: true
   validates :name, presence: true, length: { maximum: 255 }
+  validates :password, presence: true, length: { minimum: 8 } # Maximum length is enforced by has_secure_password
+
+  has_secure_password
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_13_192644) do
+ActiveRecord::Schema.define(version: 2020_06_13_194846) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
