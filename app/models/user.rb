82760ef556e17d02dbaf682037c0ae96243e840e
nicogreenarry <@1Y0#ZephFd0>
2020-06-13 11:37:26 -0700

    feat: Add more user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  validates :email, presence: true
-  validates :name, presence: true
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: /.+@.+/ }
+  validates :name, presence: true, length: { maximum: 255 }
 end
