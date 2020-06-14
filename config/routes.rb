e1e1fad1aa8c421b718da0ed2fb3eb098887ca8e
nicogreenarry <@1Y0#ZephFd0>
2020-06-13 22:13:20 -0700

    feat: Basic login endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   resources :users
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
   # Static pages
   get '/about', to: 'static_pages#about'
