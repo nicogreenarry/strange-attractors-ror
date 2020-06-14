c34a4875ee782b2522301298f99a09f51f9d9110
nicogreenarry <@1Y0#ZephFd0>
2020-06-13 20:20:53 -0700

    feat: Specify valid user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # TODO Handle a successful save.
+    else
+      render 'new'
+    end
+  end
+
+  private def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
 end
