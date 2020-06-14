0c01e24dcb8a0b49d3ea51d983e967bb72fdcc83
nicogreenarry <@1Y0#ZephFd0>
2020-06-13 20:56:38 -0700

    feat: Redirect to user profile page upon registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      # TODO Handle a successful save.
+      redirect_to @user
     else
       render 'new'
     end
