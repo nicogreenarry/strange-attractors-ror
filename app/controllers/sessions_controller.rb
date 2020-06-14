8783e7dc50e193f31eb9664e59982b6f66eff20e
nicogreenarry <@1Y0#ZephFd0>
2020-06-13 22:39:37 -0700

    feat: Working login!

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user&.authenticate(params[:session][:password])
-      # TODO: Log the user in and redirect to the user's show page.
+      reset_session
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
