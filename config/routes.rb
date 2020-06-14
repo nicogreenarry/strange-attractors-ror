4a6b8dfe8915e24e1d30ba6e375a9e7825ca2966
nicogreenarry <@1Y0#ZephFd0>
2020-06-13 21:51:00 -0700

    feat: Add login controller/routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   get '/about', to: 'static_pages#about'
   get '/what-are-strange-attractors', to: 'static_pages#what_are_strange_attractors'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   resources :users
 end
