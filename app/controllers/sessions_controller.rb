abd46c4ca633ff5427b6c7eb859ea9581e5b7e4a
nicogreenarry <@1Y0#ZephFd0>
2020-06-13 22:32:46 -0700

    feat: Warn users if they fail to log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-    render 'new'
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user&.authenticate(params[:session][:password])
+      # TODO: Log the user in and redirect to the user's show page.
+    else
+      flash.now[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
 
   def destroy
