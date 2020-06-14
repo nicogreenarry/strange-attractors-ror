81a6c4be3c46c6bc35ae6498b70a35d6c5f7700a
nicogreenarry <@1Y0#ZephFd0>
2020-06-13 22:12:58 -0700

    fix: Keep signup url from changing after failed signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   root 'homepage#index'
-  get 'signup', to: 'users#new'
+
+  # Registration and auth
+  resources :users
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+
+  # Static pages
   get '/about', to: 'static_pages#about'
   get '/what-are-strange-attractors', to: 'static_pages#what_are_strange_attractors'
-  get '/login', to: 'sessions#new'
-  post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
-  resources :users
 end
