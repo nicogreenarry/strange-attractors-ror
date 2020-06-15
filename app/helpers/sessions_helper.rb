25b74de47c9bd69d4d88bcaad1249457d9fd6093
nicogreenarry <@1Y0#ZephFd0>
2020-06-15 08:36:27 -0700

    feat: Session helpers current_User and logged_in?

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,5 +1,16 @@
 module SessionsHelper
+  def current_user
+    if session[:user_id]
+      @current_user ||= session[:user_id] && User.find_by(id: session[:user_id])
+    end
+  end
+
   def log_in(user)
     session[:user_id] = user.id
   end
+
+  def logged_in?
+    !!current_user
+  end
+
 end
