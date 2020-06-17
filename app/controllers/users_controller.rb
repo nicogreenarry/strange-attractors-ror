49bd156bb431b5c731dc17a621c40e20869a1c93
nicogreenarry <@1Y0#ZephFd0>
2020-06-17 10:16:28 -0700

    feat: Prevent logged-out users from editing profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -37,4 +39,11 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :email, :password)
   end
 
+  # Before filters
+  private def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
 end
