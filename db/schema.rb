c531020b8a7eea444ba713f54471f5dfff2a8aaf
nicogreenarry <@1Y0#ZephFd0>
2020-06-17 14:16:01 -0700

    feat: Add User.admin flag

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_212817) do
+ActiveRecord::Schema.define(version: 2020_06_17_210346) do
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_06_16_212817) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
