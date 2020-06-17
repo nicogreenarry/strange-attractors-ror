e6226614b111d97fc9a427e8f65e194b1e827462
nicogreenarry <@1Y0#ZephFd0>
2020-06-17 10:10:17 -0700

    feat: Handle successful profile updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,8 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update(user_params)
-      # TODO Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
