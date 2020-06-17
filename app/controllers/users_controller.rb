fa1bd86fc486c2106d51af9b3cf9d2589fd11ac9
nicogreenarry <@1Y0#ZephFd0>
2020-06-17 10:27:07 -0700

    feat: Prevent user from editing another user's profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def new
     @user = User.new
@@ -22,11 +23,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Profile updated"
       redirect_to @user
@@ -46,4 +45,10 @@ class UsersController < ApplicationController
       redirect_to login_url
     end
   end
+
+  private def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
+  end
+
 end
