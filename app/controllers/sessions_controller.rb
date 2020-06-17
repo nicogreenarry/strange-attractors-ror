7df317e25144ff95d3349d8dbd423452fa3cf8e2
nicogreenarry <@1Y0#ZephFd0>
2020-06-16 21:55:46 -0700

    feat: Add "Remember me" persistent login checkbox

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       reset_session
       log_in user
-      remember user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
