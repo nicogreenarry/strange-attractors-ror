b8c0a32b2df38df7b2787ed306512f25ae18104d
nicogreenarry <@1Y0#ZephFd0>
2020-06-18 15:33:52 -0700

    chore: Create AfterShip class

--- config/application.rb
+++ config/application.rb
@@ -8,12 +8,16 @@ Bundler.require(*Rails.groups)
 
 module StrangeAttractorsRor
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+
+    console do
+      require "./lib/after_ship"
+    end
   end
 end
---
+++ lib/after_ship.rb
@@ -0,0 +1,2 @@
+module AfterShip
+end
