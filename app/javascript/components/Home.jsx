a5be6c838493adee11ac0200ed0af2a7e8829792
nicogreenarry <@1Y0#ZephFd0>
2020-06-19 08:28:05 -0700

    feat: Move homepage controls into a sidebar; get rid of title

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -17,60 +17,59 @@ export default () => {
 
   return (
     <div className="primary-color d-flex flex-column align-items-center justify-content-center">
-      <header className="container secondary-color text-center">
-        <h1 className="display-4">Strange Attractors</h1>
-      </header>
-      <section>
-        <hr className="my-2" />
-        <div className="d-flex justify-content-center">
-          <button
-            className="btn btn-primary mx-2"
-            onClick={() => {
-              setTweakMode(false);
-              setCoefficientsIdx(prevIdx => (prevIdx + 1) % someGoodAttractors.length);
-            }}
-          >
-            Next saved attractor
-          </button>
-          <button
-            className="btn btn-primary mx-2"
-            onClick={() => {
-              setTweakMode(false);
-              const attractorPoints = findInterestingCoefficients();
-              setAttractorPointProps(attractorPoints);
-            }}
-          >
-            Generate new attractor
-          </button>
-        </div>
-        <hr className="my-2" />
-        <div className="d-flex justify-content-center mb-4">
-          <button
-            className="btn btn-sm btn-outline-primary mx-2"
-            onClick={() => {
-              setTweakMode(true);
-              setCacheId(prev => prev + 1);
-            }}
-          >
-            Tweak this attractor
-          </button>
-        </div>
-        {
-          tweakMode
-            ? (
-              <TweakAttractor cacheId={cacheId} {...attractorPointProps} className="mb-3" />
-            ) : (
-              <Attractor
-                {...attractorPointProps}
-                showEquation={true}
-                className="mb-3"
-                initialCount={45000}
-                width={500}
-                height={500}
-              />
-            )
-        }
+      <section className="d-flex w-100">
+        <div className="sidebar d-flex flex-column justify-content-start flex-grow-0 p-2">
+          <div className="d-flex flex-column justify-content-center border-bottom py-2">
+            <button
+              className="btn btn-outline-primary m-2"
+              onClick={() => {
+                setTweakMode(false);
+                setCoefficientsIdx(prevIdx => (prevIdx + 1) % someGoodAttractors.length);
+              }}
+            >
+              Next saved attractor
+            </button>
+            <button
+              className="btn btn-outline-primary m-2"
+              onClick={() => {
+                setTweakMode(false);
+                const attractorPoints = findInterestingCoefficients();
+                setAttractorPointProps(attractorPoints);
+              }}
+            >
+              Generate new attractor
+            </button>
+          </div>{/* /page controls */}
+          <div className="d-flex flex-column justify-content-center mb-4 py-2">
+            <button
+              className="btn btn-outline-primary m-2"
+              onClick={() => {
+                setTweakMode(true);
+                setCacheId(prev => prev + 1);
+              }}
+            >
+              Tweak this attractor
+            </button>
+          </div>{/* /attractor controls */}
+        </div>{/* /sidebar */}
+        <main className="flex-grow-1 d-flex flex-column justify-content-start align-content-center p-2">
+          {
+            tweakMode
+              ? (
+                <TweakAttractor cacheId={cacheId} {...attractorPointProps} className="mb-3" />
+              ) : (
+                <Attractor
+                  {...attractorPointProps}
+                  showEquation={true}
+                  className="mb-3"
+                  initialCount={45000}
+                  width={500}
+                  height={500}
+                />
+              )
+          }
+        </main>
       </section>
-    </div>
+    </div>// /page container
   );
 };
