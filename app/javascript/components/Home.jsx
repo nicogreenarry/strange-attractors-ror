4ca44ad36a9e885517cc9c1f7324f3c835a266dd
nicogreenarry <@1Y0#ZephFd0>
2020-06-19 16:54:29 -0700

    refactor: Use styled components for better self-documenting code

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -1,9 +1,48 @@
 import React, { useEffect, useState } from "react";
-import Attractor from './Attractor';
+import styled from 'styled-components';
+
 import findInterestingCoefficients from '../services/attractor/find_interesting_coefficients';
 import {someGoodAttractors} from '../services/attractor/saved_sets';
 
+import Attractor from './Attractor';
 import TweakAttractor from './Attractor/TweakAttractor';
+
+const PageContainer = styled.section`
+  display: flex;
+  width: 100%;
+`;
+const Sidebar = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  
+  flex-grow: 0;
+  
+  padding: 2px;
+`;
+const SidebarButtons = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 8px;
+  
+  border-bottom: solid 1px lightgray;
+  &:last-child {
+    border-bottom: none;
+  }
+`;
+const PageControls = SidebarButtons;
+const AttractorControls = SidebarButtons;
+const Main = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-content: center;
+  
+  padding: 8px;
+  
+  flex-grow: 1;
+`;
 
 export default () => {
   const [coefficientsIdx, setCoefficientsIdx] = useState(0);
@@ -16,60 +55,58 @@ export default () => {
   const [cacheId, setCacheId] = useState(0); // Ultimately this will be based on something like the history length
 
   return (
-    <div className="primary-color d-flex flex-column align-items-center justify-content-center">
-      <section className="d-flex w-100">
-        <div className="sidebar d-flex flex-column justify-content-start flex-grow-0 p-2">{/* sidebar */}
-          <div className="d-flex flex-column justify-content-center border-bottom py-2">{/* page controls */}
-            <button
-              className="btn btn-secondary m-2"
-              onClick={() => {
-                setTweakMode(false);
-                setCoefficientsIdx(prevIdx => (prevIdx + 1) % someGoodAttractors.length);
-              }}
-            >
-              Next featured attractor
-            </button>
-            <button
-              className="btn btn-secondary m-2"
-              onClick={() => {
-                setTweakMode(false);
-                const attractorPoints = findInterestingCoefficients();
-                setAttractorPointProps(attractorPoints);
-              }}
-            >
-              Generate new attractor
-            </button>
-          </div>{/* /page controls */}
-          <div className="d-flex flex-column justify-content-center mb-4 py-2">{/* attractor controls */}
-            <button
-              className="btn btn-secondary m-2"
-              onClick={() => {
-                setTweakMode(true);
-                setCacheId(prev => prev + 1);
-              }}
-            >
-              Tweak this attractor
-            </button>
-          </div>{/* /attractor controls */}
-        </div>{/* /sidebar */}
-        <main className="flex-grow-1 d-flex flex-column justify-content-start align-content-center p-2">
-          {
-            tweakMode
-              ? (
-                <TweakAttractor cacheId={cacheId} {...attractorPointProps} className="mb-3" />
-              ) : (
-                <Attractor
-                  {...attractorPointProps}
-                  showEquation={true}
-                  className="mb-3"
-                  initialCount={45000}
-                  width={500}
-                  height={500}
-                />
-              )
-          }
-        </main>
-      </section>
-    </div>// /page container
+    <PageContainer>
+      <Sidebar>
+        <PageControls>
+          <button
+            className="btn btn-secondary m-2"
+            onClick={() => {
+              setTweakMode(false);
+              setCoefficientsIdx(prevIdx => (prevIdx + 1) % someGoodAttractors.length);
+            }}
+          >
+            Next featured attractor
+          </button>
+          <button
+            className="btn btn-secondary m-2"
+            onClick={() => {
+              setTweakMode(false);
+              const attractorPoints = findInterestingCoefficients();
+              setAttractorPointProps(attractorPoints);
+            }}
+          >
+            Generate new attractor
+          </button>
+        </PageControls>
+        <AttractorControls>
+          <button
+            className="btn btn-secondary m-2"
+            onClick={() => {
+              setTweakMode(true);
+              setCacheId(prev => prev + 1);
+            }}
+          >
+            Tweak this attractor
+          </button>
+        </AttractorControls>
+      </Sidebar>
+      <Main>
+        {
+          tweakMode
+            ? (
+              <TweakAttractor cacheId={cacheId} {...attractorPointProps} className="mb-3" />
+            ) : (
+              <Attractor
+                {...attractorPointProps}
+                showEquation={true}
+                className="mb-3"
+                initialCount={45000}
+                width={500}
+                height={500}
+              />
+            )
+        }
+      </Main>
+    </PageContainer>
   );
 };
