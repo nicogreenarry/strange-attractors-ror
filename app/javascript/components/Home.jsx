03af7c44b8ea8087860ccbced7f42dd733fbf1b1
nicogreenarry <@1Y0#ZephFd0>
2020-06-18 19:02:20 -0700

    refactor: Use semantic html

--- app/javascript/components/Home.jsx
+++ app/javascript/components/Home.jsx
@@ -17,58 +17,60 @@ export default () => {
 
   return (
     <div className="primary-color d-flex flex-column align-items-center justify-content-center">
-      <div className="container secondary-color text-center">
+      <header className="container secondary-color text-center">
         <h1 className="display-4">Strange Attractors</h1>
         <p className="lead">
           Math is beautiful!
         </p>
-      </div>
-      {
-        tweakMode
-          ? (
-            <TweakAttractor cacheId={cacheId} {...attractorPointProps} className="mb-3" />
-          ) : (
-            <Attractor
-              {...attractorPointProps}
-              showEquation={true}
-              className="mb-3"
-              initialCount={45000}
-              width={500}
-              height={500}
-            />
-          )
-      }
+      </header>
+      <section>
+        {
+          tweakMode
+            ? (
+              <TweakAttractor cacheId={cacheId} {...attractorPointProps} className="mb-3" />
+            ) : (
+              <Attractor
+                {...attractorPointProps}
+                showEquation={true}
+                className="mb-3"
+                initialCount={45000}
+                width={500}
+                height={500}
+              />
+            )
+        }
 
-      <div className="flex mb-4">
-        <button
-          className="btn btn-primary mx-2"
-          onClick={() => {
-            setTweakMode(false);
-            setCoefficientsIdx(prevIdx => (prevIdx + 1) % someGoodAttractors.length);
-          }}
-        >
-          Next saved attractor
-        </button>
-        <button
-          className="btn btn-outline-primary mx-2"
-          onClick={() => {
-            setTweakMode(false);
-            const attractorPoints = findInterestingCoefficients();
-            setAttractorPointProps(attractorPoints);
-          }}
-        >
-          Generate new attractor
-        </button>
-        <button
-          className="btn btn-outline-primary mx-2"
-          onClick={() => {
-            setTweakMode(true);
-            setCacheId(prev => prev + 1);
-          }}
-        >
-          Tweak this attractor
-        </button>
-      </div>
+        <div className="flex mb-4">
+          <button
+            className="btn btn-primary mx-2"
+            onClick={() => {
+              setTweakMode(false);
+              setCoefficientsIdx(prevIdx => (prevIdx + 1) % someGoodAttractors.length);
+            }}
+          >
+            Next saved attractor
+          </button>
+          <button
+            className="btn btn-outline-primary mx-2"
+            onClick={() => {
+              setTweakMode(false);
+              const attractorPoints = findInterestingCoefficients();
+              setAttractorPointProps(attractorPoints);
+            }}
+          >
+            Generate new attractor
+          </button>
+          <button
+            className="btn btn-outline-primary mx-2"
+            onClick={() => {
+              setTweakMode(true);
+              setCacheId(prev => prev + 1);
+            }}
+          >
+            Tweak this attractor
+          </button>
+        </div>
+      </section>
     </div>
   );
 };
