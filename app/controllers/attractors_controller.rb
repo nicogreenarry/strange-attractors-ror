dcfb15b075951ffbcacffa5a2c5ca786c65516dd
nicogreenarry <@1Y0#ZephFd0>
2020-06-19 20:54:47 -0700

    feat: Endpoint for fetching a random featured attractor

--- app/controllers/attractors_controller.rb
+++ app/controllers/attractors_controller.rb
@@ -1,2 +1,28 @@
 class AttractorsController < ApplicationController
+  def random_featured
+    query = if Rails.env.development? # dev uses sqlite
+              <<~SQL
+                SELECT * FROM attractors
+                WHERE featured_at IS NOT NULL
+                LIMIT 1
+                OFFSET cast(
+                  (random() / 18446744073709551614.0 + 0.5)
+                    * (SELECT COUNT(*) FROM attractors WHERE featured_at IS NOT NULL) AS INTEGER
+                )
+              SQL
+            else
+              <<~SQL
+                SELECT * FROM attractors
+                WHERE featured_at IS NOT NULL
+                LIMIT 1
+                OFFSET floor(
+                  random() * (SELECT COUNT(*) FROM attractors WHERE featured_at IS NOT NULL)
+                )
+              SQL
+            end
+
+    attractor = ActiveRecord::Base.connection.execute(query).first
+
+    render json: attractor.slice('id', 'details')
+  end
 end
