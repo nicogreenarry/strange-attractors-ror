d75920dc35fb1bb78eab0d5fa8bfcc3a90044007
nicogreenarry <@1Y0#ZephFd0>
2020-06-27 15:39:07 -0700

    feat: Create pw reset controller

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  resources :password_resets, only: [:new, :create, :edit, :update]
 
   # Attractors
   get '/attractors/featured/random', to: 'attractors#random_featured'
