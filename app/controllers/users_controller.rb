84fa656361609f307037681c924b9553fc230e76
nicogreenarry <@1Y0#ZephFd0>
2020-07-03 20:59:28 -0700

    refactor: De-dup logged_in_user Before Filter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,14 +55,6 @@ class UsersController < ApplicationController
   end
 
   # Before filters
-  private def logged_in_user
-    unless logged_in?
-      store_location
-      flash[:danger] = "Please log in."
-      redirect_to login_url
-    end
-  end
-
   private def correct_user
     @user = User.find(params[:id])
     redirect_to(root_url) unless current_user?(@user)
